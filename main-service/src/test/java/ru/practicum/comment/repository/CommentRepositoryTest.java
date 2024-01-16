a000e3eca3d6070f6380a2b0a30b102c8490a09a
Воронков Никита <nikitavoronkov@yandex.ru>
2024-01-16 17:38:07 +0300

    front: add js

--- main-service/src/test/java/ru/practicum/comment/repository/CommentRepositoryTest.java
+++ main-service/src/test/java/ru/practicum/comment/repository/CommentRepositoryTest.java
@@ -1,7 +1,7 @@
 package ru.practicum.comment.repository;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
